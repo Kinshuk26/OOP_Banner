be59c6ec686a3d7849197f56fb9b79ee80493457
Kinshuk26 <kinshukj26@gmail.com>
2026-03-02 08:50:31 +0530

    Made Changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,20 @@
 /* 
-@version 4.0
+@version 5.0
 @autor Kinshuk
  */
-public class HelloWorld{
+public class HelloWorld {
  public static void main(String args[]){
-    String[] lines = new String[7];
-     lines[0] =  ("   ***   " + "  " + "   ***  " + "  " + " ****** " + "  " + "   *****");
-     lines[1] = (" **   **" + "  " + "  **   **" + "  " + " **   **" + "  " + " **");
-     lines[2] = ("**     **" + "  " + "**     **" + "  " + "**   **" + "  "+ "**");
-     lines[3] = ("**     **" + "  " + "**     **" + "  " + "****** " + "  " + "  *****");
-     lines[4] = ("**     **" + "  " +  "**     **" + "  " + "**    " + "  " + "      **");
-     lines[5] = (" **   **" + "  " + "  **   **" + "  " + " **    " + "  " + " **   **");
-     lines[6] = ("   ***   " + "  " + "   ***   " + "  " + "**    " + "  " + "  *****    ");
+    String[] lines = {
+     String.join("","   ***   " , "  " , "   ***  " , "  " , " ****** " , "  " , "   *****"),
+     String.join(""," **   **" , "  " , "  **   **" , "  "  , " **   **" , "  " , " **"),
+     String.join("","**     **" , "  " , "**     **" , "  " , "**   **" , "  ", "**"),
+     String.join("","**     **" , "  " , "**     **" , "  " , "****** " , "  " , "  *****"),
+     String.join("","**     **" , "  " ,  "**     **" , "  ", "**    " , "  " , "      **"),
+     String.join(""," **   **" , "  " , "  **   **" , "  "  , " **    " , "  " , " **   **"),
+     String.join("","   ***   " , "  " , "   ***   " , "  " , "**    " , "  " , "  *****    "),
+    };
      for (String line : lines){
-        System.out.println(line);
+        System.out.println(line); 
      }
     }
 }
